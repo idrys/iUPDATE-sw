ce745a5c0b37cf3a7742c8f1ff68a509bc104fbd
idrys <idrys2@gmail.com>
2017-10-05 14:43:20 +0200

    W pełni działający

--- iUPDATE-sw/Form1.cs
+++ iUPDATE-sw/Form1.cs
@@ -3,18 +3,50 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace iUPDATE_sw
 {
     public partial class Form1 : Form
     {
+        string pathToCad;
+        
         public Form1()
         {
+            pathToCad = String.Empty;
             InitializeComponent();
+          
+        }
+
+        public string PathToCAD
+        {
+            get { return pathToCad; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            pathToCad = this.openFileDialog1.FileName;
+            this.Close();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            this.openFileDialog1.FileName = "iUPDATE.exe";
+            this.openFileDialog1.ShowDialog();
+            this.textBox1.Text = this.openFileDialog1.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
--- iUPDATE-sw/Program.cs
+++ iUPDATE-sw/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -8,15 +10,108 @@ namespace iUPDATE_sw
 {
     static class Program
     {
+        static string pathToCAD;
         /// <summary>
         /// Główny punkt wejścia dla aplikacji.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            pathToCAD = FindPathToCadDECOR();
+            if ( pathToCAD == String.Empty)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Form1 form1 = new Form1();
+                Application.Run(form1);
+                pathToCAD = form1.PathToCAD;
+            }
+
+            InstalliUPDATE(pathToCAD);
+
+
+
         }
+
+        private static void InstalliUPDATE(string pathTo_iUPDATE_EXE)
+        {
+            string currentPath = System.IO.Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath);
+
+            if (pathTo_iUPDATE_EXE == String.Empty)
+            {
+                MessageBox.Show("Nie znaleziono ścieżki do CadDEcor");
+                return;
+            }
+
+            //StreamReader file = File.OpenText();
+
+            string pathToiUPDATE_bat = Path.ChangeExtension(pathTo_iUPDATE_EXE, "bat");
+            string pathToCad = Path.GetDirectoryName(pathTo_iUPDATE_EXE) ;
+            string pathDesktopCurrentUser = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); 
+
+            MoveFile(currentPath + "\\iUPDATE.ex_", pathToCad + "\\iUPDATE.exe");
+            MoveFile(currentPath + "\\SendEmailsw.ex_", pathToCad + "\\SendEmailsw.exe");
+           
+            File.AppendAllText(pathToiUPDATE_bat, "\"" + pathTo_iUPDATE_EXE + "\"");
+            File.AppendAllText(pathToiUPDATE_bat, " | ");
+            File.AppendAllText(pathToiUPDATE_bat, "\"" + pathToCad + "\\SendEmailsw.exe\"");
+
+            ShortcutHelper.CreateShortcut(
+                 pathDesktopCurrentUser + "\\Aktualizacja CADDecor.lnk",
+                 "C:\\Windows\\System32\\runas.exe",
+                 "c:\\Users\\Admin\\Desktop\\",
+                 " /env /user:Admin /savecred \"" + pathToiUPDATE_bat + "\""
+                 );
+            Process proc = new Process();
+            proc.StartInfo.FileName = pathDesktopCurrentUser + "\\Aktualizacja CADDecor.lnk";
+            proc.Start();
+        }
+
+        private static bool MoveFile(string pathSrc, string pathDest)
+        {
+            if (File.Exists(pathDest))
+                File.Delete(pathDest);
+
+            if (File.Exists(pathSrc))
+                File.Copy(pathSrc, pathDest);
+            else
+            {
+                MessageBox.Show("Nie znalazłem SenEmailsw.exe");
+                return false;
+            }
+            return true;
+        }
+
+        private static string FindPathToCadDECOR()
+        {
+            string[] paths =
+            {
+                "c:\\CadProjekt\\CAD Decor Paradyz v. 2.5.0\\iUPDATE.exe",
+                "d:\\CadProjekt\\CAD Decor Paradyz v. 2.5.0\\iUPDATE.exe",
+                "c:\\CadProjekt\\CAD Decor Paradyz v. 2.4.0\\iUPDATE.exe",
+                "d:\\CadProjekt\\CAD Decor Paradyz v. 2.4.0\\iUPDATE.exe",
+                "c:\\CadProjekt\\CAD Decor Paradyz v. 2.3.0\\iUPDATE.exe",
+                "d:\\CadProjekt\\CAD Decor Paradyz v. 2.3.0\\iUPDATE.exe",
+                "c:\\CadProjekt\\CAD Decor Paradyz v. 2.2.0\\iUPDATE.exe",
+                "d:\\CadProjekt\\CAD Decor Paradyz v. 2.2.0\\iUPDATE.exe",
+                "c:\\CadProjekt\\CAD Decor Paradyz v. 2.1.0\\iUPDATE.exe",
+                "d:\\CadProjekt\\CAD Decor Paradyz v. 2.1.0\\iUPDATE.exe",
+                "c:\\CadProjekt\\CAD Decor Paradyz v. 2.0.0\\iUPDATE.exe",
+                "d:\\CadProjekt\\CAD Decor Paradyz v. 2.0.0\\iUPDATE.exe",
+                "c:\\CadProjekt\\iUPDATE.exe",
+                "d:\\CadProjekt\\iUPDATE.exe"
+
+            };
+
+            foreach (var path in paths)
+            {
+                if (File.Exists(path))               
+                    return path;
+            }
+
+            return String.Empty; 
+        }
+
     }
 }
